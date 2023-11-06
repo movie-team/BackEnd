f3de99315f600e2156dc1a604e346e94df2ce7df
권기용 <badacura@gmail.com>
2023-11-06 17:15:18 +0900

    movies urls 작성, likes url accounts->movies로 이동

--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', ),
+    path('', views.index),
+    path('<int:movie.pk>/', views.detail),
+    path('<int:movie.pk>/review/<int:review.pk>/likes/<int:user.pk>/', views.likes),
+    path('<int:movie.pk>/ticketing/<int:user.pk>/', views.ticketing),
+    path('worldcup/', views.worldcup),
 ]
