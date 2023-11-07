84b63d020bf36f96424f09b55b5368b0e87a4095
ssw <ssw4627@gmail.com>
2023-11-07 16:42:49 +0900

    231107_신시원_01

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+app_name = 'movies'
 
 urlpatterns = [
-    path('', views.index),
-    path('<int:movie.pk>/', views.detail),
-    path('<int:movie.pk>/review/<int:review.pk>/likes/<int:user.pk>/', views.likes),
-    path('<int:movie.pk>/ticketing/<int:user.pk>/', views.ticketing),
-    path('worldcup/', views.worldcup),
+    path('', views.index, name='index'),
+    path('<int:movie.pk>/', views.detail, name='detail'),
+    path('review/<int:review.pk>/likes/', views.likes, name='likes'),
+    path('<int:movie.pk>/ticketing/', views.ticketing, name='ticketing'),
+    path('worldcup/', views.worldcup, name='worldcup'),
 ]
