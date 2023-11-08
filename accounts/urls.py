71de06c4eb40d377c7d9c5f249e1476ff36cb2d2
sirlyun <sirlyun23@gmail.com>
2023-11-08 21:22:24 +0900

    ✨feat: 토큰 유효성/재발급/test url 생성

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 from . import views
 
 urlpatterns = [
     path('login/', views.login),
     # path('<int:user.pk>/logout/', views.logout),
     path('signup/', views.signup),
+    # jwt 토큰 재발급
+    path('auth/refresh/', TokenRefreshView.as_view()),
+    path('auth/verify', TokenVerifyView.as_view()),
+    path('test/', views.test)
     # path('<int:user.pk>/delete/', views.delete),
     # path('<int:user.pk>/profile/', views.profile),
 ]
