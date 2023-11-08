be5ba3ba76611a6a61e27976a76ee63208a62776
sirlyun <sirlyun23@gmail.com>
2023-11-08 16:55:15 +0900

    ✨feat: 에러 방지 미작성 함수 주석 처리

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login),
-    path('<int:user.pk>/logout/', views.logout),
+    # path('<int:user.pk>/logout/', views.logout),
     path('signup/', views.signup),
-    path('<int:user.pk>/delete/', views.delete),
-    path('<int:user.pk>/profile/', views.profile),
+    # path('<int:user.pk>/delete/', views.delete),
+    # path('<int:user.pk>/profile/', views.profile),
 ]
