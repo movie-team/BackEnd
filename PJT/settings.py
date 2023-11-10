bace3e10f11146c9ab797623daecdbddbbcf6888
sirlyun <sirlyun23@gmail.com>
2023-11-10 16:37:35 +0900

    ✨feat: cors 설정 추가

--- PJT/settings.py
+++ PJT/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    # cors 설정
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -59,7 +61,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
+    'allauth.account.middleware.AccountMiddleware',
+    # cors 설정
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'PJT.urls'
@@ -194,3 +198,8 @@ AUTHENTICATION_BACKENDS = [
 SITE_ID = 1  # Django-allauth를 사용할 때 필요한 설정
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 LOGIN_REDIRECT_URL = '/'  # 로그인 후 리다이렉트될 URL
+
+CORS_ALLOWED_ORIGINS = [
+    # 허용할 클라이언트 주소
+    "http://localhost:5173",  
+]
