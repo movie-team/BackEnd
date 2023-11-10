a0e17c8fcb08b553716c1da846d7e67c6355de91
sirlyun <sirlyun23@gmail.com>
2023-11-10 15:21:44 +0900

    ✨feat: 회원탈퇴, 로그아웃, 회원 정보 변경 코드 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,12 +4,17 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login),
-    # path('<int:user.pk>/logout/', views.logout),
+    path('logout/', views.logout),
     path('signup/', views.signup),
     # jwt 토큰 재발급
     path('auth/refresh/', TokenRefreshView.as_view()),
+    # jwt 토큰 유효성 진단
     path('auth/verify', TokenVerifyView.as_view()),
-    path('test/', views.test)
-    # path('<int:user.pk>/delete/', views.delete),
-    # path('<int:user.pk>/profile/', views.profile),
+
+    path('signout/', views.signout),
+    path('update/', views.update),
+    path('password/', views.password),
+    path('', views.profile),
+
+    # path('google/')
 ]
