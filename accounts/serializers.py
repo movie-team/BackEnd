557ea9a3de0f8a74000f22dfe9b6613cf7727d53
sirlyun <sirlyun23@gmail.com>
2023-11-14 22:50:17 +0900

    ✨feat: email 추가

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -8,6 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(
+            username = validated_data['username'],
             email = validated_data['email'],
             password = validated_data['password']
         )
