45655cc9a4723cabf75ef4cdf65a65bed9ddc872
sirlyun <sirlyun23@gmail.com>
2023-11-14 22:49:47 +0900

    ✨feat: username 필드  부활

--- accounts/models.py
+++ accounts/models.py
@@ -1,21 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password, **kwargs):
-        if not email:
-            raise ValueError('Users must have an email address')
+    def create_user(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError('Users must have an username address')
         user = self.model(
-            email = email,
+            username=username,
+            **extra_fields
         )
         user.set_password(password)
         user.save(using = self._db)
         return user
     
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('email', 'sirlyun23@gmail.com')
 
-class User(AbstractBaseUser):
+        return self.create_user(username, password, **extra_fields)
+
     
-    email = models.EmailField(max_length=30, unique=True, null=False, blank=False)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=20, unique=True, null=False, blank=False)
+    email = models.EmailField(max_length=30, blank=False)
     first_name = models.CharField(max_length=20, default=False, blank=True)
     last_name = models.CharField(max_length=20, default=False, blank=True)
     is_superuser = models.BooleanField(default=False)
@@ -27,5 +36,4 @@ class User(AbstractBaseUser):
 	# 헬퍼 클래스 사용
     objects = UserManager()
 
-	# 사용자의 username field는 email으로 설정 (이메일로 로그인)
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'username'
